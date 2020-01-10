fd6b5de43b2a8a9a638789b2e9a6168ea464fc26
CMen_ <dfnil@live.ru>
2020-01-10 18:04:32 +0500

    Initial commit

---
+++ IttTti/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace IttTti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor =
+                ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top |
+                                                         System.Windows.Forms.AnchorStyles.Bottom) |
+                                                        System.Windows.Forms.AnchorStyles.Left) |
+                                                       System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(749, 197);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor =
+                ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Bottom |
+                                                        System.Windows.Forms.AnchorStyles.Left) |
+                                                       System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 215);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(909, 275);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor =
+                ((System.Windows.Forms.AnchorStyles) ((System.Windows.Forms.AnchorStyles.Top |
+                                                       System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(766, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(766, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(766, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(766, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 496);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(908, 21);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ IttTti/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IttTti
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image1;
+        int x;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(Object sender, EventArgs e)
+        {
+            image1 = new Bitmap(x, x);
+
+            progressBar1.Maximum = richTextBox1.Text.Length;
+
+            int i = 0;
+
+            for (int row = 0; row < image1.Width; row++)
+            {
+                for (int column = 0; column < image1.Height; column++)
+                {
+                    int pos = (++i - 1) * 3;
+
+                    int r = (int) richTextBox1.Text[pos];
+                    int g = (int) richTextBox1.Text[++pos];
+                    int b = (int) richTextBox1.Text[++pos];
+
+                    if (r > 255) r = 0;
+                    if (g > 255) g = 0;
+                    if (b > 255) b = 0;
+
+                    Color newColor = Color.FromArgb(r, g, b);
+                    image1.SetPixel(column, row, newColor);
+
+                    if (pos + 3 >= richTextBox1.Text.Length)
+                    {
+                        row = image1.Width;
+                        column = image1.Height;
+                    }
+
+                    progressBar1.Value = pos;
+
+                    pictureBox1.Image = image1;
+
+                    Application.DoEvents();
+                }
+            }
+
+            pictureBox1.Image = image1;
+            image1.Save("myfile.bmp", ImageFormat.Bmp);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int count = richTextBox1.Text.Length;
+            label1.Text = $@"Символов: {count}";
+
+            int div = count % 3;
+            if (div == 1) count += 2;
+            else if (div == 2) count += 1;
+            x = (int) (Math.Ceiling(Math.Sqrt(count / 3)));
+
+            label2.Text = $@"Остаток: {div}";
+            label3.Text = $"X*Y: {x}";
+        }
+    }
+}
